70cbb69a4be6c891744ee8cdad86a376dcc0820b
Anthony Fu <anthonyfu117@hotmail.com>
2016-09-13 15:28:13 +0800

    Add instance check

--- src/InputRemote.Client.Receiver/Program.cs
+++ src/InputRemote.Client.Receiver/Program.cs
@@ -13,9 +13,24 @@ namespace InputRemote.Client.Receiver
         [STAThread]
         static void Main()
         {
+
+            bool result;
+            // Mutex is uesd for instance check
+            var mutex = new System.Threading.Mutex(true, "inputremote-client-receiver", out result);
+
+            if (!result)
+            {
+                MessageBox.Show("Another instance is already running.");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
+
+            // mutex shouldn't be released - important line
+            GC.KeepAlive(mutex);                
+            
         }
     }
 }
